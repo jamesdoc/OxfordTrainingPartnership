4f1d83324cebf589a2f8765a6cfdfaad07dc9be6
Doc <hi@jamesdoc.com>
2014-07-17 18:36:47 +0100

    Adding in a very basic Vagrant file

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Go install a LAMP stack
+sudo apt-get update
+sudo apt-get -q -y install php5-gd apache2 php5
+
+# Configure apache to look at the correct folder, and enable symlinks
+rm -rf /var/www
+ln -fs /vagrant/www /var/www
+a2enmod rewrite
+sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
+service apache2 restart
